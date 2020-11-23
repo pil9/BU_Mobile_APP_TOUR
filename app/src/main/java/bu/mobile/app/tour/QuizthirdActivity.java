559f43be25e5441c08bcf39bb690c3f51d9270c1
gimbeomjung <rla5451@naver.com>
2020-11-24 00:46:51 +0900

    퀴즈 3문제 프론트 추가

---
+++ app/src/main/java/bu/mobile/app/tour/QuizthirdActivity.java
@@ -0,0 +1,14 @@
+package bu.mobile.app.tour;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuizthirdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quizthird);
+    }
+}
