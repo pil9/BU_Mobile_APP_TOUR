0234adf2959ab8230e82e9bfe94cadc4dad78de9
KimSunKyu <pyr04@naver.com>
2020-12-03 20:50:32 +0900

    qr코드 액티비티 수정

--- app/src/main/java/bu/mobile/app/tour/QRcodeActivity.java
+++ app/src/main/java/bu/mobile/app/tour/QRcodeActivity.java
@@ -18,15 +18,18 @@ import com.google.zxing.integration.android.IntentResult;
 public class QRcodeActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button scanBtn;
+    Button intent_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_q_rcode);
 
         scanBtn = findViewById(R.id.scanBtn);
         scanBtn.setOnClickListener(this);
+
     }
+
 
     @Override
     public void onClick(View view) {
@@ -39,18 +42,49 @@ public class QRcodeActivity extends AppCompatActivity implements View.OnClickLis
         integrator.setCaptureActivity(CaptureAct.class);
         integrator.setOrientationLocked(false);
         integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
-        integrator.setPrompt("Scanning Code");
+        integrator.setPrompt("코드 스캔 중");
         integrator.initiateScan();
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null){
+            if(result.getContents() != null){
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setMessage(result.getContents());
+                builder.setTitle("결과 스캔 중");
+                builder.setPositiveButton("다시 스캔해주세요", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        scanCode();
+                    }
+                }).setPositiveButton("쿠폰보기", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
 
-        IntentResult result = IntentIntegrator.parseActivityResult(requestCode,
+                        Intent intent = new Intent(QRcodeActivity.this,MypageActivity.class);
+                        startActivity(intent);
+                        finish();
 
-                resultCode, data);
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(result.getContents()));
-        startActivity(intent);
+
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
+            }
+            else if(Patterns.WEB_URL.matcher(result.getContents()).matches()){
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(result.getContents()));
+                startActivity(browserIntent);
+            }
+            else {
+                Toast.makeText(this, "결과가 없습니다", Toast.LENGTH_LONG).show();
+            }
+        }else{
+            super.onActivityResult(requestCode, resultCode, data);
+
+        }
 
     }
+
 }
