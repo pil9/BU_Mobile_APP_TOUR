7f24dc8d5721b9ef33b37f8a9ccd7e9c6e654457
rla5451 <rla5451@naver.com>
2020-12-04 21:05:38 +0900

    마이페이지 회원디비  연동 후 아이디 출력

--- app/src/main/java/bu/mobile/app/tour/MypageActivity.java
+++ app/src/main/java/bu/mobile/app/tour/MypageActivity.java
@@ -60,7 +60,7 @@ public class MypageActivity extends Fragment {
 
         myHelper=new bu.mobile.app.tour.LoginActivity.myDBHelper(getActivity());
 
-        useridx = 1;//로그인 페이지와 연동 후 확인 필요 임시값 입력함
+        //useridx = 1;//로그인 페이지와 연동 후 확인 필요 임시값 입력함
         sqlDB2=myHelper.getReadableDatabase();
         Cursor cursor2;
         cursor2=sqlDB2.rawQuery("SELECT * FROM member WHERE midx='"+useridx+"' ;",null);//select문 실행
